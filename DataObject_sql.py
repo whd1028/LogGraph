64d92b5ada506694be777e6344cb46518fb66067
whd1028 <zmfhqk101101@naver.com>
2022-02-14 18:41:59 +0900

    LogGraph와 DataObject_sql작성

--- DataObject_sql.py
+++ DataObject_sql.py
@@ -1,6 +1,7 @@
 #dataObject
 from datetime import datetime
 from SqlConn import SqlConn
+from typing import List
 
 class Log:
 
@@ -9,14 +10,43 @@ class Log:
         self.acstime:datetime = acstime
         self.url:str = url
 
+
     @staticmethod
-    def selectAll() -> list:
+    def selectData(user_id:str =None, ip: str =None, time1: str =None, time2: str =None, limit: int=None, order: str ='DESC') -> list :
+        if not any([user_id, ip, time1, time2, limit]):
+            return Log.selectAll()
+        elif user_id != None:
+            return Log.selectByUserID(user_id)
+        elif ip != None:
+            return Log.selectByip(ip, limit=limit)
+        elif time1 != None:
+            return Log.selectBytime(time1, time2, order=order)
+        ...
+
+    @staticmethod
+    def selectByUserID(user_id: str) -> list:
+        cursor = SqlConn.Cursor()
+        query = f"select * from Log where id = '{user_id}'"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url in results:
+            object_list.append(Log(ip, acstime, url))
+
+        return object_list
+        ...
+
+    @staticmethod
+    def selectAll() -> None:
         cursor = SqlConn.Cursor()
         query = f"select * from Log "
         cursor.execute(query)
-        result = cursor.fetchall()
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url in results:
+            object_list.append(Log(ip, acstime, url))
 
-        return result
+        return object_list
 
     @staticmethod
     def selectByip(ip:str, limit:int=None) -> list:
@@ -27,9 +57,12 @@ class Log:
         else:    
             query = f"select * from Log where IPaddr = '{ip}'"
         cursor.execute(query)
-        result = cursor.fetchall()
-
-        return result
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url in results:
+            object_list.append(Log(ip, acstime, url))
+            
+        return object_list
 
     @staticmethod
     def selectBytime(time1:str, time2:str=None, order:str='DESC') -> list:
@@ -41,9 +74,12 @@ class Log:
             query = f"select * from Log where acstime > '{time1}' and acstime < '{time2}' order by acstime {order}"
             ...
         cursor.execute(query)
-        result = cursor.fetchall()
-
-        return result
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url in results:
+            object_list.append(Log(ip, acstime, url))
+            
+        return object_list
     
     
 class Scroll_Data():
@@ -53,38 +89,85 @@ class Scroll_Data():
         self.url = url
         self.staytime = staytime
         self.scroll = scroll
+
+    @staticmethod
+    def selectData(ip: str =None, url: str =None, n_id: str =None, limit: int =None) -> list or None:
+        if not any([ip, url, n_id]):
+            return Scroll_Data.selectAll()
+        elif ip != None:
+            return Scroll_Data.selectByip(ip=ip, limit=limit)
+        elif url != None:
+            return Scroll_Data.selectByURL(url=url)
+        elif n_id != None:
+            return Scroll_Data.selectByn_id(n_id=n_id)
+        ...
     
+
     @staticmethod
     def selectAll() -> list:
         cursor = SqlConn.Cursor()
         query = f"select * from Scroll_Data"
         cursor.execute(query)
-        result = cursor.fetchall()
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url, staytime, scroll in results:
+            object_list.append(Scroll_Data(ip, acstime, url, staytime, scroll))
 
-        return result
+        return object_list
+
+    @staticmethod
+    def selectByip(ip:str, limit:int=None) -> list :
+        cursor = SqlConn.Cursor()
+        # query = str.format("select * from Scroll_Data where ipaddr = {0}",ip)
+        if limit != None:
+            query = f"select * from Scroll_Data where ipaddr = '{ip}' limit {limit}"
+        else:    
+            query = f"select * from Scroll_Data where ipaddr = '{ip}'"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url, staytime, scroll in results:
+            object_list.append(Scroll_Data(ip, acstime, url, staytime, scroll))
+
+        return object_list
 
     @staticmethod
     def selectByURL(url:str)-> list:
         cursor = SqlConn.Cursor()
         query = f"select * from Scroll_Data where URL = '{url}'"
         cursor.execute(query)
-        result = cursor.fetchall()
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url, staytime, scroll in results:
+            object_list.append(Scroll_Data(ip, acstime, url, staytime, scroll))
 
-        return result
+        return object_list
 
     @staticmethod
     def selectByn_id(n_id:str) -> list:
         cursor = SqlConn.Cursor()
         query = f"select * from Scroll_Data where URL = 'news/news_post/{n_id}'"
         cursor.execute(query)
-        result = cursor.fetchall()
+        results = cursor.fetchall()
+        object_list = []
+        for ip, acstime, url, staytime, scroll in results:
+            object_list.append(Scroll_Data(ip, acstime, url, staytime, scroll))
 
-        return result
+        return object_list
+
+    @staticmethod
+    def selectNewsRank(n_id:str) -> None:
+        ...
     ...
 
 if __name__ == "__main__":
-    # print(len(Log.selectByip('127.0.0.1')))
+    # print(Log.selectByip('127.0.0.1'))
     # print(len(Log.selectAll()))
-
+    # print(Log.selectData())
+    print(__name__, __package__, __spec__, __annotations__, __file__, __builtins__)
+    check = Log.__mro__
+    check2 = Log.selectAll.__qualname__
+    # print( [] == None)
+    # Log.tt()
+    Log.selectAll()
     ...
-    
--- LogGraph.py
+++ LogGraph.py
@@ -1,10 +1,63 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from DataObject_sql import Log, Scroll_Data
+
 
 class LogGraph():
-    ...
+    
+    @staticmethod  # 사용자(개인)이 선호하는 언론사
+    def make_graph_individual_like_press():
+        
+        ...
+    
+    @staticmethod  # 사용자(개인)이 선호하는 카테고리
+    def make_graph_individual_like_category():
+        ...
+
+    @staticmethod  # 사용자 성별 선호하는 뉴스기사
+    def make_graph_gender_like_news():
+        ...
+
+    @staticmethod  # 연령대별 선호하는 뉴스기사
+    def make_graph_age_like_news():
+        ...
+
+
+    @staticmethod
+    def select_Data(dataObject:str, **keyarg) -> pd.DataFrame:
+        str = f"{dataObject}.selectData(**{keyarg})"
+        return eval(str)
+        ...
+
+    @staticmethod
+    def make_poltGraph(data, x, y) -> None:
+        data_list = LogGraph.select_Data(data)
+        ...
+
+    @staticmethod
+    def make_barGraph() -> None:
+        ...
+
+    @staticmethod
+    def make_pieGraph() -> None:
+        ...
+
+    @staticmethod
+    def show_Graph() -> None:
+        ...
+
+    @staticmethod
+    def save_Graph(path: str) -> None:
+        ...
 
 if __name__ == "__main__":
+    # print(LogGraph.select_Data("Log",ip="183.98.215.85",limit=100))
+    data_list = LogGraph.select_Data("Log")
+    plot_df = pd.DataFrame()
+    for i in data_list:
+        plot_df.append(i.__dict__, ignore_index=True)
+    print(plot_df)
+
     ...
 
